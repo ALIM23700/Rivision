fe271e6e8db63b9c118f7d202a843fc6d6e4eb48
abdul alim <abdulalim23700@gmail.com>
2026-08-18 03:45:41 +0600

    javascript and react

---
+++ javascript/practice/practice.js
@@ -0,0 +1,73 @@
+//console.log(10);
+//alert("alim vai")
+//document.write("alimvai")
+//var eggName=25;
+//console.log(eggName);
+//var eggName="rahim";
+//var isTrue=true;
+//console.log(eggName);
+//console.log(typeof(isTrue))
+//-------arrat-----
+//var publlics=[50000,100,300,400,500,1000];
+//var frindsName=["alim","salim","dalim","valim"];
+//console.log(publlics)
+//console.log(frindsName)
+//console.log(typeof(publlics))
+//console.log(publlics.length)
+//console.log(publlics.indexOf(300))
+//console.log(publlics[3])
+//publlics[0]=33;
+//console.log(publlics)
+//frindsName.push("kalam");
+//frindsName.pop("kalam");
+//console.log(frindsName)
+//publlics.unshift(8)
+//publlics.shift()
+//publlics.sort()
+//console.log(publlics)
+//--------object-----------
+/*var alim={
+    name:"alim",
+    age:25,
+    isgood:true,
+    interest:["frontend","backend"],
+    alim2:{age:22,name:"someyhing"}
+
+}*/
+//alim.frinds=["rahim","karim"]
+//delete alim.name
+//console.log(alim)
+//console.log(alim.age)
+//console.log(alim["name"])
+//console.log(typeof(alim))
+//console.log(typeof(publlics))
+//console.log(alim.alim2.age)
+//---------null , undefined-----------
+//var x;
+//var y=null;
+//console.log(x);
+//console.log(typeof(x));
+//console.log(y);
+//console.log(typeof(y));
+
+//----premative vs non premative
+/* premative:number,bolian non-premative:array,object,function*/
+
+//--------let,var,const-----------
+/* var x=20;
+var x=25;
+console.log(x); */
+//let x=20;
+//let x=25;//cannot do redeclare
+//x=34//resign possible
+//const x=20;//cannot redeclare
+//const x=25;//cannot reassign
+//console.log(x);
+//console.log(x);
+//--------mathematical operation------------
+//let x=10
+//let y=20
+//let z=x+y;
+//console.log(z);//cabe minus,multiply,division
+
+
